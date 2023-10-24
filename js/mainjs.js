d9fa005ef7de8dc7efaad9bbad591b7d920a5be5
thanhoaiedunlu <21130360@st.hcmuaf.edu.vn>
2023-10-24 13:25:57 +0700

    update login

--- js/mainjs.js
+++
@@ -1,9 +0,0 @@
-const addressbtn = document.querySelector('.tinh')
-const addressclose = document.querySelector('#address-close')
-// console.log(addressbtn)
-addressbtn.addEventListener("click",function(){
-    document.querySelector('.address-form').style.display = "flex"
-})
-addressclose.addEventListener("click",function(){
-    document.querySelector('.address-form').style.display = "none"
-})
