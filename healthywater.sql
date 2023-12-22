975962bbc8f980a7fc94d1c645e09d8feab833db
huydoan0212 <21130374@st.hcmuaf.edu.vn>
2023-12-22 18:50:14 +0700

    fix main

--- healthywater.sql
+++ healthywater.sql
@@ -40,7 +40,7 @@ CREATE TABLE `products` (
                             `updated_at` date
 );
 
-CREATE TABLE `galerys` (
+CREATE TABLE `gallerys` (
                            `id` int PRIMARY KEY,
                            `product_id` int,
                            `img` varchar(255)
@@ -165,7 +165,7 @@ ALTER TABLE `products` ADD FOREIGN KEY (`category_id`) REFERENCES `categorys` (`
 
 ALTER TABLE `order_details` ADD FOREIGN KEY (`product_id`) REFERENCES `products` (`id`);
 
-ALTER TABLE `galerys` ADD FOREIGN KEY (`product_id`) REFERENCES `products` (`id`);
+ALTER TABLE `gallerys` ADD FOREIGN KEY (`product_id`) REFERENCES `products` (`id`);
 
 ALTER TABLE `order_details` ADD FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`);
 
@@ -194,3 +194,21 @@ ALTER TABLE `products` ADD FOREIGN KEY (`sale_product_id`) REFERENCES `timesale_
 ALTER TABLE `rates` ADD FOREIGN KEY (`product_id`) REFERENCES `products` (`id`);
 
 ALTER TABLE `rates` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+
+INSERT INTO `categorys` (`id`, `name`) VALUES (1, 'Lọc nước RO');
+INSERT INTO `categorys` (`id`, `name`) VALUES (2, 'Có nóng lạnh');
+INSERT INTO `categorys` (`id`, `name`) VALUES (3, 'Âm tủ để bàn');
+INSERT INTO `categorys` (`id`, `name`) VALUES (4, 'Lọc nước Nano, UF, MF');
+INSERT INTO `categorys` (`id`, `name`) VALUES (5, 'Lọc nước không điện');
+
+INSERT INTO `products` (`id`, `category_id`, `type_machine_id`, `brand_id`, `sale_product_id`, `title`, `price`, `discount_price`, `description`, `available`, `created_at`, `updated_at`, `img`) VALUES (0, 1, NULL, NULL, NULL, 'Máy lọc nước RO nóng nguội lạnh Karofi KAD-X39 10 lõi', 10490000, 6190000, 'Máy lọc nước', 100, '2023-12-18', '2023-12-18', 'img/ro-nong-lanh-karofi-kad-x39-10-loi-140223-034144-600x600.jpg');
+INSERT INTO `products` (`id`, `category_id`, `type_machine_id`, `brand_id`, `sale_product_id`, `title`, `price`, `discount_price`, `description`, `available`, `created_at`, `updated_at`, `img`) VALUES (1, 1, NULL, NULL, NULL, 'Máy lọc nước RO Mutosi MP-290SK 9 lõi', 6670000, 3490000, 'Máy lọc nước', 100, '2023-12-10', '2023-12-10', 'img/ro-mutosi-mp-290sk-9-loi-140223-054510-600x600-1-600x600.jpg');
+INSERT INTO `products` (`id`, `category_id`, `type_machine_id`, `brand_id`, `sale_product_id`, `title`, `price`, `discount_price`, `description`, `available`, `created_at`, `updated_at`, `img`) VALUES (2, 1, NULL, NULL, NULL, 'Máy lọc nước RO nóng nguội lạnh Karofi KAD-X58 11 lõi', 12660000, 8690000, 'Máy lọc nước', 50, '2023-12-11', '2023-12-11', 'img/may-loc-nuoc-ro-nong-nguoi-lanh-karofi-kad-x58-11-loi-0-600x600.jpg');
+INSERT INTO `products` (`id`, `category_id`, `type_machine_id`, `brand_id`, `sale_product_id`, `title`, `price`, `discount_price`, `description`, `available`, `created_at`, `updated_at`, `img`) VALUES (3, 1, NULL, NULL, NULL, 'Máy lọc nước RO nóng nguội lạnh Hòa Phát HWBS3A 1021 10 lõi', 10670000, 6490000, 'Máy lọc nước', 2002, '2023-12-16', '2023-12-16', 'img/may-loc-nuoc-ro-nong-nguoi-lanh-hoa-phat-hwbs3a-1021-10-loi-210623-023624-600x600.jpg');
+INSERT INTO `products` (`id`, `category_id`, `type_machine_id`, `brand_id`, `sale_product_id`, `title`, `price`, `discount_price`, `description`, `available`, `created_at`, `updated_at`, `img`) VALUES (4, 1, NULL, NULL, NULL, 'Máy lọc nước RO nóng nguội lạnh Kangaroo KG10A12 10 lõi', 10790000, 6990000, 'Máy lọc nước', 5, '2023-12-16', '2023-12-16', 'img/may-loc-nuoc-ro-nong-nguoi-lanh-kangaroo-kg10a12-10-loi-050723-040500-600x600.jpg');
+
+INSERT INTO `gallerys` (`id`, `product_id`, `img`) VALUES (1, 0, 'img/may-loc-nuoc-ro-karofi-kaq-x16-10-loi-130223-054043-600x600.jpg');
+INSERT INTO `gallerys` (`id`, `product_id`, `img`) VALUES (2, 0, 'img/small3.jpg');
+INSERT INTO `gallerys` (`id`, `product_id`, `img`) VALUES (3, 0, 'img/small2.jpg');
+INSERT INTO `gallerys` (`id`, `product_id`, `img`) VALUES (4, 0, 'img/small1.jpg');
