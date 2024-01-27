430565899e683d01351d51ae6d01980fd5cd8024
huydoan0212 <21130374@st.hcmuaf.edu.vn>
2024-01-27 16:15:54 +0700

    admin

--- src/main/java/controller/PaymentOrderController.java
+++ src/main/java/controller/PaymentOrderController.java
@@ -1,5 +1,6 @@
 package controller;
 
+import dao.OrderDAO;
 import model.Order;
 import service.OrderService;
 import service.PaymentService;
@@ -20,12 +21,23 @@ public class PaymentOrderController extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        Order order = (Order) req.getSession().getAttribute("order");
+        Object object = req.getSession().getAttribute("order_id");
+        int order_id = 0;
+        if (object != null) {
+            if (object instanceof Integer) {
+                order_id = (Integer) object;
+            } else if (object instanceof String) {
+                order_id = Integer.valueOf((String) object);
+            }
+        }
+        Order order = OrderDAO.getOrderById(order_id);
+        System.out.println(order);
+        System.out.println(order_id);
         String payment_type = req.getParameter("paymentOption");
-        boolean isPayment = OrderService.getInstance().paymentOrder("Đã chọn phương thức thanh toán", order.getId());
+        boolean isPayment = OrderService.getInstance().paymentOrder("Đã chọn phương thức thanh toán", order_id);
         boolean isInsertPayment = PaymentService.getInstance().insertPayment(order.getId(), payment_type, order.getTotal_money());
         if (isPayment && isInsertPayment) {
-            req.getSession().removeAttribute("cart");
+            req.getSession().removeAttribute("order_id");
             resp.sendRedirect("trangchu");
         }
     }
