aeba74df175666a0ccc5bd9077f6b2b83b79a05b
ThanhHoai <21130360@st.hcmuaf.edu.vn>
2024-05-25 20:01:20 +0700

    login google

--- src/main/java/controller/LoginGoogle.java
+++ src/main/java/controller/LoginGoogle.java
@@ -1,5 +1,8 @@
 package controller;
 
+import model.GoogleAccount;
+import service.Google;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,12 +13,30 @@ import java.io.IOException;
 public class LoginGoogle extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        doPost(req, resp);
+        processRequest(req, resp);
     }
-
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+        processRequest(req, resp);
+    }
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        String code = request.getParameter("code");
+        String error = request.getParameter("error");
+        //neu nguoi dung huy uy quyen
+        if(error != null) {
+            request.getRequestDispatcher("login.jsp").forward(request, response);
+        }
+        Google gg = new Google();
+        String accessToken = gg.getToken(code);
+        GoogleAccount acc = gg.getUserInfo(accessToken);
+        //check tk da dky chua
+        System.out.println(acc);
+    }
+    @Override
+    public String getServletInfo() {
+        return "Short description";
     }
 
 }
---
+++ src/main/java/service/Iconstant.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package service;
+
+/**
+ *
+ * @author HP
+ */
+public class Iconstant {
+    public static final String GOOGLE_CLIENT_ID = "192822040743-18a4tpap0e863adnqs3rh60ro2e33qip.apps.googleusercontent.com";
+    public static final String GOOGLE_CLIENT_SECRET = "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";
+    public static final String GOOGLE_REDIRECT_URI = "http://localhost:8080/ProjectLTW_war_exploded/loginGoogle";
+    public static final String GOOGLE_GRANT_TYPE = "authorization_code";
+    public static final String GOOGLE_LINK_GET_TOKEN = "https://accounts.google.com/o/oauth2/token";
+    public static final String GOOGLE_LINK_GET_USER_INFO = "https://www.googleapis.com/oauth2/v1/userinfo?access_token=";
+}
